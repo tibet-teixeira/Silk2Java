ab575595b837b76bafad0d83bae9aba5be5339eb
Tibet Teixeira <tibetbrasil@gmail.com>
2019-04-04 16:57:25 -0200

    Added new classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import Read.ReadXMLFile;
 import Tags.*;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.util.ArrayList;
 
@@ -8,11 +11,25 @@ public class Main {
     public static void main (String argv[]) {
 
         try {
-            String pathname = "linkSpec.xml";
+            String pathname = "linkSpec2.xml";
 
             Document xml = new ReadXMLFile(pathname).getDocument();
 
             xml.getDocumentElement().normalize();
+
+            Element rootNode = (Element) xml.getChildNodes().item(0); // get Root node
+            ArrayList<String> tags = new ArrayList<>();
+
+            NodeList tagsChild = rootNode.getChildNodes();
+            for (int index = 0; index < tagsChild.getLength(); index++) {
+                if (! tagsChild.item(index).getNodeName().equals("#text")) {
+                    tags.add(tagsChild.item(index).getNodeName());
+                }
+            }
+
+            for(String tag:tags){
+                System.out.println(tag);
+            }
 
             Prefixes prefixes = new Prefixes(xml);
             Interlinks interlinks = new Interlinks(xml);
@@ -20,19 +37,19 @@ public class Main {
             ArrayList<Prefix> listPrefixes = prefixes.getPrefixes();
             ArrayList<Interlink> listInterlinks = interlinks.getInterlinks();
 
-            for (Interlink interlink : listInterlinks) {
-                DatasetProp source = interlink.getSourceDataset();
-                DatasetProp target = interlink.getTargetDataset();
-                System.out.println("Id: " + interlink.getId());
-                System.out.println("LinkType: " + interlink.getLinkType());
-                System.out.println("SourceData: " + source.getDataSource() +
-                        " - " + source.getRestrictTo() +
-                        " - " + source.getVar());
-                System.out.println("TargetData: " + target.getDataSource() +
-                        " - " + target.getRestrictTo() +
-                        " - " + target.getVar());
-                System.out.println("\n\n\n");
-            }
+//            for (Interlink interlink : listInterlinks) {
+//                DatasetProp source = interlink.getSourceDataset();
+//                DatasetProp target = interlink.getTargetDataset();
+//                System.out.println("Id: " + interlink.getId());
+//                System.out.println("LinkType: " + interlink.getLinkType());
+//                System.out.println("SourceData: " + source.getDataSource() +
+//                        " - " + source.getRestrictTo() +
+//                        " - " + source.getVar());
+//                System.out.println("TargetData: " + target.getDataSource() +
+//                        " - " + target.getRestrictTo() +
+//                        " - " + target.getVar());
+//                System.out.println("\n");
+//            }
 
         } catch (Exception e) {
             e.printStackTrace();
